1cc0f63a79333330796d303fbf9f2130f20ebfbd
Joerg Thienenkamp <33601046+jthien@users.noreply.github.com>
2025-09-09 12:20:19 +0000

    Add sql check

--- call_agent.py
+++ call_agent.py
@@ -2,6 +2,7 @@ from google import auth as google_auth
 from google.auth.transport import requests as google_requests
 import requests
 import json
+import os
 
 def get_identity_token():
     credentials, _ = google_auth.default()
@@ -9,18 +10,40 @@ def get_identity_token():
     credentials.refresh(auth_request)
     return credentials.token
 
-response = requests.post(
-    f"https://us-central1-aiplatform.googleapis.com/v1/projects/vodaf-hack25dus-903/locations/us-central1/reasoningEngines/7261706953460547584:query",
-    headers={
-        "Content-Type": "application/json; charset=utf-8",
-        "Authorization": f"Bearer {get_identity_token()}",
-    },
-    data=json.dumps({
-        "class_method": "query",
-        "input": {
-            "input": "What is the exchange rate from US dollars to Swedish Krona today?"
-        }
-    })
-    print (data)
-)
+def check_sql_rule_agent(sql_file):
+    with open(sql_file, 'r') as f:
+        content = f.read()
+        
+    response = requests.post(
+        f"https://us-central1-aiplatform.googleapis.com/v1/projects/vodaf-hack25dus-903/locations/us-central1/reasoningEngines/7261706953460547584:query",
+        headers={
+            "Content-Type": "application/json; charset=utf-8",
+            "Authorization": f"Bearer {get_identity_token()}",
+        },
+        data=json.dumps({
+            "class_method": "query",
+            "input": {
+                "input": f"SQL Stype check for: {content}"
+            }
+        })
+    )
+    
+    return response
+
+
+def process_sql_files(root_folder):
+    for foldername, subfolders, filenames in os.walk(root_folder):
+        for filename in filenames:
+            if filename.endswith('.sql'):
+                print (f"Style check for {filename}")
+                file_path = os.path.join(foldername, filename)
+                
+                response = check_sql_rule_agent(file_path)
+                data = json.loads(response.text)
+
+                # Print output.output
+                print(data["output"]["output"])
+                
+
+process_sql_files('sql')
 
